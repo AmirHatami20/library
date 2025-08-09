32b27182b76febac77af735f77317faba046bd11
Amir Hatami <hatamiamir055@email.com>
2025-08-09 20:46:15 +0330

    Add shuffled books

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,13 @@ import {bookService} from "@/service";
 
 export default async function HomePage() {
     const books = await bookService.getAll()
+    const shuffledBooks = [...books].sort(() => Math.random() - 0.5)
 
     return (
         <MotionWrapper>
             <Hero {...books?.[0]} />
-            <SwiperWrapper items={books} title="محبوب‌ترین کتاب‌ها"/>
             <SwiperWrapper items={books} title="جدیدترین کتاب‌ها"/>
+            <SwiperWrapper items={shuffledBooks} title="محبوب‌ترین کتاب‌ها"/>
         </MotionWrapper>
     );
 }
